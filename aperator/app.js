25e4cef7b54c70e151785e50ecb17970e2f0c2b4
Yunus <128239091+Botirkhoja@users.noreply.github.com>
2023-05-22 19:04:18 +0300

    Add files via upload

---
+++ aperator/app.js
@@ -0,0 +1,61 @@
+// matematik va arifmetik belgilar
+
+
+// let son1 = 15260;
+// let son2 = 15
+
+// let summa = son1 / son2;
+
+// console.log(summa)
+
+
+// let a = 20;
+// let b = 9;
+
+// let summa = 20 % 9;
+
+// console.log(summa)
+
+
+// let misol = 20;
+
+// console.log(--misol);
+
+// increment sonni kopaytirib beradi ++   misol uchun 10 dep yozsak 11 qilib beradi
+
+//  decrement sonni bittaga kopaytirib beradi -- misol uchun 10 bosa 9 qilib beradi
+
+
+// let a = 5;
+// let b = "5";
+
+// console.log(a == b);
+
+// == trini xisobga omidi va ularni tenglab beradi
+
+// === turini hisobga olgan holda ularni tenglab beradi
+
+
+// let a = 5;
+// let b = 7;
+
+// console.log(a != b);
+
+// != bu teng emas belgisi
+
+
+// let son1 = 40;
+// let son2 = 47;
+
+// console.log(son1 > son2);
+
+// <  > kotta va kichik osha osha matematikadagi belgilar
+
+
+// let letter = 20;
+
+// console.log(letter >= 50);
+
+let a = 150;
+
+console.log(a <= 150);
